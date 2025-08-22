9945b90b30b0d9067fc94d61a39c6d286d2f118c
niat786 <niat786@gmail.com>
2025-08-22 22:06:21 +0500

    app setup fdfd

--- components/QRCodeGenerator.tsx
+++ components/QRCodeGenerator.tsx
@@ -110,7 +110,6 @@ export default function QRCodeGenerator() {
                 data={qrData}
                 customization={customization}
               />
-
               <DownloadPanel
                 canvasRef={qrCanvasRef}
                 customization={customization}
