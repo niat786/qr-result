c864cd225ff71d52bfd727758367b52d60e43db7
niat786 <niat786@gmail.com>
2026-02-09 02:05:12 +0500

    v2 app

--- app/manifest.ts
+++ app/manifest.ts
@@ -23,13 +23,13 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/icon-192.png',
                 sizes: '192x192',
                 type: 'image/png',
-                purpose: 'any maskable',
+                purpose: 'any maskable' as any,
             },
             {
                 src: '/icon-512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'any maskable',
+                purpose: 'any maskable' as any,
             },
         ],
         screenshots: [
